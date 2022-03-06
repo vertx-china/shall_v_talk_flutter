1844b24a44bf2a2285393d10b5e77907e915631c
ChenPengHuang <330775121@qq.com>
2022-03-06 21:43:36 +0800

    重连逻辑、emoji选择器

--- lib/module/message/message_provider.dart
+++ lib/module/message/message_provider.dart
@@ -1,3 +1,5 @@
+import 'package:emojis/emoji.dart';
+import 'package:emojis/emojis.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shall_v_talk_flutter/base/base_change_notifier.dart';
@@ -23,6 +25,7 @@ class MessageProvider extends BaseChangeNotifier {
 
   void _initCallback() {
     VTalkSocketClient.client.addMessageReceiveCallback(_onMessageReceive);
+    VTalkSocketClient.client.addConnectStateChangeCallback(_onConnectStateChange);
   }
 
   void _onMessageReceive(Message message) {
@@ -30,14 +33,33 @@ class MessageProvider extends BaseChangeNotifier {
     notifyListeners();
   }
 
+  void _onConnectStateChange(bool connected){
+    this.connected = connected;
+    notifyListeners();
+  }
+
+  void appendEmoji(Emoji emoji){
+    textEditingController.text = textEditingController.text + emoji.char;
+  }
+
   Future<void> sendMessage(String message) async {
     scrollController.jumpTo(0.0);
     var vTalkProvider = context.read<VTalkProvider>();
     var nickname = vTalkProvider.nickname;
     textEditingController.clear();
+    String assembled = Emoji.assemble([Emojis.man, Emojis.man, Emojis.girl, Emojis.boy]);
     Message data = VTalkSocketClient.client.sendMessage(message);
     data.nickname = nickname;
     messages.insert(0, data);
+    notifyListeners();
+  }
+
+  void changePanelVisibleState(int index) {
+    if(displayPanelIndex == index){
+      displayPanelIndex = -1;
+    }else{
+      displayPanelIndex = index;
+    }
     notifyListeners();
   }
 }
